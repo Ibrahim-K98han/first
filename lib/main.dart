a58cb41e913a72efa53efd002d24ac4a8e81f710
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-06-19 18:26:56 +0600

    code modified

--- lib/main.dart
+++ lib/main.dart
@@ -13,80 +13,91 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: Scaffold(
-        // appBar: AppBar(
-        //   title: Text("My First App"),
-        // ),
-        body:Column(
-          children: [
-              Image.asset('img/sajek.jpg', width: double.infinity,height: 300.0,fit: BoxFit.cover,),
-              buildTitleSetcion(),
-              buildNavSection(),
-              Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text('Sajek valley is known for its natural environment and is surrounded by mountains, dense forest, and grassland hill tracks. Many small rivers flow through the mountains among which the Kachalong and the Machalong are notable. On the way to Sajek valley, one has to cross the Mayni range and the Mayni river.'),
+      home: HomePage(),
+    );
+  }
 
-              ),
-          ],
-        ),
+
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text("My First App"),
+      // ),
+      body:Column(
+        children: [
+          Image.asset('img/sajek.jpg', width: double.infinity,height: 300.0,fit: BoxFit.cover,),
+          buildTitleSetcion(),
+          buildNavSection(),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Text('Sajek valley is known for its natural environment and is surrounded by mountains, dense forest, and grassland hill tracks. Many small rivers flow through the mountains among which the Kachalong and the Machalong are notable. On the way to Sajek valley, one has to cross the Mayni range and the Mayni river.'),
+
+          ),
+        ],
+      ),
+    );
+  }
+  Padding buildNavSection() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Icon(Icons.call, color: Colors.blue,),
+              SizedBox(height: 10,),
+              Text('CALL',style: TextStyle(fontSize: 18),),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.near_me,color: Colors.blue),
+              SizedBox(height: 10,),
+              Text('ROUTE',style: TextStyle(fontSize: 18),),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.share,color: Colors.blue),
+              SizedBox(height: 10,),
+              Text('SHARE',style: TextStyle(fontSize: 18),),
+            ],
+          )
+        ],
       ),
     );
   }
 
-  Padding buildNavSection() {
-    return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Icon(Icons.call, color: Colors.blue,),
-                      SizedBox(height: 10,),
-                      Text('CALL',style: TextStyle(fontSize: 18),),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.near_me,color: Colors.blue),
-                      SizedBox(height: 10,),
-                      Text('ROUTE',style: TextStyle(fontSize: 18),),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(Icons.share,color: Colors.blue),
-                      SizedBox(height: 10,),
-                      Text('SHARE',style: TextStyle(fontSize: 18),),
-                    ],
-                  )
-                ],
-              ),
-            );
-  }
-
   Padding buildTitleSetcion() {
     return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Sajek Valley', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                      Text('Khagrachori, Bandharban', style: TextStyle(fontSize: 16),)
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.star),
-                      Text(90.toString(), style: TextStyle(fontSize: 18),),
-                    ],
-                  )
-                ],
-              ),
-            );
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Sajek Valley', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+              Text('Khagrachori, Bandharban', style: TextStyle(fontSize: 16),)
+            ],
+          ),
+          Row(
+            children: [
+              Icon(Icons.star),
+              Text(90.toString(), style: TextStyle(fontSize: 18),),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
+
 
