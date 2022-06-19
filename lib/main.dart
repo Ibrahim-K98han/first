d640a8337345f5e47355af2ac85a0bf9a333cfaf
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-06-19 18:21:48 +0600

    code refector

--- lib/main.dart
+++ lib/main.dart
@@ -20,56 +20,8 @@ class MyApp extends StatelessWidget {
         body:Column(
           children: [
               Image.asset('img/sajek.jpg', width: double.infinity,height: 300.0,fit: BoxFit.cover,),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Sajek Valley', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                        Text('Khagrachori, Bandharban', style: TextStyle(fontSize: 16),)
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Icon(Icons.star),
-                        Text(90.toString(), style: TextStyle(fontSize: 18),),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        Icon(Icons.call, color: Colors.blue,),
-                        SizedBox(height: 10,),
-                        Text('CALL',style: TextStyle(fontSize: 18),),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(Icons.near_me,color: Colors.blue),
-                        SizedBox(height: 10,),
-                        Text('ROUTE',style: TextStyle(fontSize: 18),),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(Icons.share,color: Colors.blue),
-                        SizedBox(height: 10,),
-                        Text('SHARE',style: TextStyle(fontSize: 18),),
-                      ],
-                    )
-                  ],
-                ),
-              ),
+              buildTitleSetcion(),
+              buildNavSection(),
               Padding(
                 padding: EdgeInsets.all(16.0),
                 child: Text('Sajek valley is known for its natural environment and is surrounded by mountains, dense forest, and grassland hill tracks. Many small rivers flow through the mountains among which the Kachalong and the Machalong are notable. On the way to Sajek valley, one has to cross the Mayni range and the Mayni river.'),
@@ -79,6 +31,62 @@ class MyApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Padding buildNavSection() {
+    return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.call, color: Colors.blue,),
+                      SizedBox(height: 10,),
+                      Text('CALL',style: TextStyle(fontSize: 18),),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.near_me,color: Colors.blue),
+                      SizedBox(height: 10,),
+                      Text('ROUTE',style: TextStyle(fontSize: 18),),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(Icons.share,color: Colors.blue),
+                      SizedBox(height: 10,),
+                      Text('SHARE',style: TextStyle(fontSize: 18),),
+                    ],
+                  )
+                ],
+              ),
+            );
+  }
+
+  Padding buildTitleSetcion() {
+    return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Sajek Valley', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                      Text('Khagrachori, Bandharban', style: TextStyle(fontSize: 16),)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.star),
+                      Text(90.toString(), style: TextStyle(fontSize: 18),),
+                    ],
+                  )
+                ],
+              ),
+            );
   }
 }
 
